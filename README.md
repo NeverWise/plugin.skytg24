0a94d705d23f74c04a2de940ad70e1b9c97cb6f0
NeverWise <matteopomi@libero.it>
2017-01-15 13:04:19 +0100

    Version 1.0.3 - Small bugfix/improvements.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-plugin.video.skytg24
-==============
-
-Kodi unofficial plugin for Sky Tg24 (tested on XBMC 13.1 Gotham).
+# plugin.video.skytg24
+Kodi unofficial plugin for Sky Tg24.
--- default.py
+++ default.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
-import os
-from neverwise import Util
+import neverwise as nw, os#, datetime
 
-li = Util.createListItem(Util._addonName, thumbnailImage = '{0}/icon.png'.format(os.path.dirname(os.path.abspath(__file__))), streamtype = 'video', infolabels = { 'title' : Util._addonName })
-# rtmp://cp49989.live.edgefcs.net:1935/live/streamRM1@2564 app=live playpath=streamRM1@2564 swfUrl=http://videoplatform.sky.it/player/swf/player_v2.swf pageURL=http://video.sky.it/news/diretta swfVfy=true live=true timeout=30 flashVer=LNX 11,2,202,297
-xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play('rtmp://cp49989.live.edgefcs.net:1935/live/streamRM1@2564 live=true', li)
+
+class SkyTg24(object):
+
+  def __init__(self):
+
+    li = nw.createListItem(nw.addonName, thumbnailImage = '{0}/icon.png'.format(os.path.dirname(os.path.abspath(__file__))), streamtype = 'video', infolabels = { 'title' : nw.addonName })
+    # rtmp://cp49989.live.edgefcs.net:1935/live/streamRM1@2564 app=live playpath=streamRM1@2564 swfUrl=http://videoplatform.sky.it/player/swf/player_v2.swf pageURL=http://video.sky.it/news/diretta swfVfy=true live=true timeout=30 flashVer=LNX 11,2,202,297
+    xbmc.Player().play('rtmp://cp49989.live.edgefcs.net:1935/live/streamRM1@2564 live=true', li)
+
+# Entry point.
+#startTime = datetime.datetime.now()
+st24 = SkyTg24()
+del st24
+#xbmc.log('{0} azione {1}'.format(nw.addonName, str(datetime.datetime.now() - startTime)))
